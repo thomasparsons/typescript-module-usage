a5adf1495c0681a460ff82e3997965833480a502
Tom Parsons <tom.parsons@fundingoptions.com>
2022-04-14 20:53:16 +0100

    not needed

--- src/functions/groupAndCountImports.js
+++ src/functions/groupAndCountImports.js
@@ -1,21 +1,4 @@
 //
-function sortObject(obj) {
-  var arr = [];
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop)) {
-      arr.push({
-        key: prop,
-        value: obj[prop],
-      });
-    }
-  }
-  arr.sort(function (a, b) {
-    return a.value - b.value;
-  });
-  //arr.sort(function(a, b) { a.value.toLowerCase().localeCompare(b.value.toLowerCase()); }); //use this to sort as strings
-  return arr; // returns array
-}
-
 const groupAndCountImports = (config, files, imports) => {
   const groupedAndCounted = [];
 
