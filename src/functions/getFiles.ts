4c43a8cd13e85b2188c8f78f5ad1f23d6c3cd445
thomasparsons <thomaspatrickparsons@gmail.com>
2022-04-15 11:08:09 +0100

    write output to json

--- src/functions/getFiles.ts
+++ src/functions/getFiles.ts
@@ -1,10 +1,11 @@
 //
 import glob from "glob";
+import path from "path";
 import { Config } from "../types";
 
 const getFiles = (config: Config): string[] => {
-  return glob.sync(config.filePath, {
-    root: config.baseUrl,
+  return glob.sync(path.join(process.cwd(), config.filePath), {
+    // root: config.baseUrl,
     ignore: [
       "**/*.stories*",
       "**/*.test*",
--- src/functions/output.ts
+++ src/functions/output.ts
@@ -1,19 +1,11 @@
+//
 import fs from "fs";
 import path from "path";
 
-const output = (groupedAndCounted) => {
-  console.log("groupedAndCounted", groupedAndCounted);
-  const outputJson = JSON.stringify(groupedAndCounted);
-  fs.writeFile(
+const output = async (groupedAndCounted) => {
+  fs.writeFileSync(
     path.join(process.cwd(), "output.json"),
-    outputJson,
-    (err, result) => {
-      if (err) {
-        console.log("error", err);
-        return;
-      }
-      return result;
-    }
+    JSON.stringify(groupedAndCounted)
   );
 };
 
